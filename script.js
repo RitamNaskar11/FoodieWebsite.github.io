014ed0699e0e92271ce5a2e6bfb795856786d546
Ritam Naskar <ritamnaskar.bca.2022@gmail.com>
2025-09-24 11:39:59 +0530

    First commit

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 /* script.js */
 
-const menuNav = document.getElementById('menuNav')
-const menuContainer = document.getElementById('menuContainer')
-document.getElementById("menu-toggle").addEventListener("click", () => {
-  document.getElementById("navmenu").classList.toggle("active");
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
 });
 
 const darkToggle = document.getElementById("darkToggle");
